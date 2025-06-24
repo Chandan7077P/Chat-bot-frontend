ebb456e21ae5d89059dc048a0160471465058547
Chandan7077P <chandansbi007@gmail.com>
2025-06-24 10:56:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Chatbot from "../components/Chatbot";
 export default function Home() {
   return (
     <main>
+      <meta name="google-site-verification" content="JWrF5qOlGuZNPso_KRlMKkUEDLpmZMtgcsRWiKYHbIU" />
       <Chatbot />
     </main>
   );
